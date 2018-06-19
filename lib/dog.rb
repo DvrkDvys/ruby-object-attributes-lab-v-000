031c50c78e2d4e72b2d7204e7f48f5277eb8864e
Jordan Harris <jharris@wework.com>
2018-06-19 15:40:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
   end
  
   def name
-    @dogs_name
+    @dogs_namename
   end
   
   def breed=(breed_name)
@@ -15,28 +15,3 @@ class Dog
     @dogs_breed
   end
 end
-
-# def initialize(name)
-#     @name = name
-#   end
- 
-#   def name
-#     @name
-#   end
- 
-#   def name=(new_name)
-#     @name = new_name
-#   end
-
-
-fido = Dog.new("Fido")
-fido.name
-
-# fido.instance_variable_set(:@name, "Yeezy")
-
-
-snoopy = Dog.new
-# snoopy.breed = "Beagle"
-
-
-puts fido.name
