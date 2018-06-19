690fe25b113a5f996c06dae3f9fdcc35b817ff0a
Jordan Harris <jharris@wework.com>
2018-06-19 15:39:17 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,17 +16,17 @@ class Dog
   end
 end
 
- def initialize(name)
-    @name = name
-  end
+# def initialize(name)
+#     @name = name
+#   end
  
-  def name
-    @name
-  end
+#   def name
+#     @name
+#   end
  
-  def name=(new_name)
-    @name = new_name
-  end
+#   def name=(new_name)
+#     @name = new_name
+#   end
 
 
 fido = Dog.new("Fido")
