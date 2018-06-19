d78dab758b7eb07fb9835c40e152501972453b74
Jordan Harris <jharris@wework.com>
2018-06-19 15:49:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,18 +3,11 @@ class Dog
     @dogs_name = name
   end
  
-  def name
-    @dogs_name
-  end
+  # def name
+  #   @dogs_name
+  # end
 end
 
 
 fido = Dog.new("Fido")
 puts fido.name
-
-class Fred
-  def initialize(p1, p2)
-    @a, @b = p1, p2
-  end
-end
-fred = Fred.new('cat', 99)
