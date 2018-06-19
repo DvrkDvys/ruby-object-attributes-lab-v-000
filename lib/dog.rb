f58e9902b79af4bdd62cadd165805c5b70102cae
Jordan Harris <jharris@wework.com>
2018-06-19 15:30:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
   def name=(dogs_name)
-    @this_dogs_name = dogs_name
+    @dogs_name = dogs_name
   end
  
   def name
-    @this_dogs_name
+    @dogs_name
   end
   
   def breed=(breed_name)
-    @this_dogs_breed = dogs_breed
+    @dogs_breed = dogs_breed
   end
   
   def breed 
-    @this_dogs_breed
+    @dogs_breed
   end
 end
  
