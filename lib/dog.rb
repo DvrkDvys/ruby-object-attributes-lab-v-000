52037c1c9f4401ffc7a51f58b5e4a26d95d34dee
Jordan Harris <jharris@wework.com>
2018-06-19 15:26:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,34 +7,21 @@ class dog
   def name
     @this_dogs_name
   end
+  
+  def breed=(breed_name)
+    @this_dogs_breed = dogs_breed
+  end
+  
+  def breed 
+    @this_dogs_breed
+  end
 end
  
-lassie = Dog.new
-lassie.name = "Lassie"
+fido = Dog.new
+fido.name = "Fido"
  
 puts lassie.name
 
-
-
-#   context 'with names' do
-#     describe '#name=' do
-#       it 'writes the name of the dog to an instance variable @name' do
-#         fido = Dog.new
-#         fido.name = "Fido"
-
-#         expect(fido.instance_variable_get(:@name)).to eq("Fido")
-#       end
-#     end
-
-#     describe '#name' do
-#       it 'reads the name of the dog from an instance variable @name' do
-#         fido = Dog.new
-#         fido.instance_variable_set(:@name, "Fido")
-
-#         expect(fido.name).to eq("Fido")
-#       end
-#     end
-#   end
 
 #   context 'with breeds' do
 #     describe '#breed=' do
